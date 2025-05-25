32f4299a577658daac938ce88c5d1fff6eb0bd2a
Hisoka <gintama2009@hotmail.ca>
2025-05-25 13:42:45 +0000

    testing pre-commit code

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,7 @@ const server = fastify()
 server.get('/ping', async (request, reply) => {
     return 'pong\n'
 })
-
+console.log("this is a test to see if husky works")
 server.listen({ port: 8080 }, (err, address) => {
     if (err) {
         console.error(err)
