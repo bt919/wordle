bd7ea352b902c71f98f986e786e7ab213a4f6d5f
Hisoka <gintama2009@hotmail.ca>
2025-05-25 13:57:03 +0000

    add logging and fix husky pre-commit hook

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,17 @@
-import fastify from 'fastify'
+import fastify from "fastify"
 
-const server = fastify()
-
-server.get('/ping', async (request, reply) => {
-    return 'pong\n'
+const server = fastify({
+    logger: true,
 })
-console.log("this is a test to see if husky works")
+
+server.get("/ping", async (request, reply) => {
+    return "pong\n";
+})
+
 server.listen({ port: 8080 }, (err, address) => {
     if (err) {
-        console.error(err)
-        process.exit(1)
+        console.error(err);
+        process.exit(1);
     }
     console.log(`Server listening at ${address}`)
 })
