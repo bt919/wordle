0e642df6afb346259480e09d8b3f082b506ae1f1
Hisoka <gintama2009@hotmail.ca>
2025-05-30 00:38:59 +0000

    get normal wordle mode working with the backend (state resets after page exit)

--- frontend/src/components/GuessBar.tsx
+++ frontend/src/components/GuessBar.tsx
@@ -1,61 +1,135 @@
 import clsx from "clsx";
+import { twMerge } from "tailwind-merge";
 import { useEffect, useState } from "react";
 
-export function GuessBar({ guess }: { guess: string }) {
+export function GuessBar({
+	guess,
+	correctLetters,
+	misplacedLetters,
+}: { guess: string; correctLetters: number[]; misplacedLetters: number[] }) {
 	useEffect(() => {}, []);
 
 	// bg-gray-600
 	return (
 		<div className="flex gap-2">
 			<div
-				className={clsx(
-					"h-16 w-16 border-2 text-slate-200 text-4xl font-[1000] flex justify-center items-center select-none",
-					{
-						"border-gray-400": guess.length > 0,
-					},
-					{
-						"border-gray-600": guess.length <= 0,
-					},
+				className={twMerge(
+					clsx(
+						"h-16 w-16 border-2 text-slate-200 text-4xl font-[1000] flex justify-center items-center select-none",
+						{
+							"border-gray-400": guess.length > 0,
+						},
+						{
+							"border-gray-600": guess.length <= 0,
+						},
+						{
+							"bg-gray-600 border-slate-950": guess.length === 5,
+						},
+						{
+							"bg-[#B59F38]": misplacedLetters.indexOf(0) !== -1,
+						},
+						{
+							"bg-green-600": correctLetters.indexOf(0) !== -1,
+						},
+					),
 				)}
 			>
 				{guess.length > 0 ? guess.charAt(0) : ""}
 			</div>
 			<div
-				className={clsx(
-					"h-16 w-16 border-2 text-slate-200 text-4xl font-[1000] flex justify-center items-center select-none",
-					{
-						"border-gray-400": guess.length > 1,
-					},
-					{
-						"border-gray-600": guess.length <= 1,
-					},
+				className={twMerge(
+					clsx(
+						"h-16 w-16 border-2 text-slate-200 text-4xl font-[1000] flex justify-center items-center select-none",
+						{
+							"border-gray-400": guess.length > 1,
+						},
+						{
+							"border-gray-600": guess.length <= 1,
+						},
+						{
+							"bg-gray-600 border-slate-950": guess.length === 5,
+						},
+						{
+							"bg-[#B59F38]": misplacedLetters.indexOf(1) !== -1,
+						},
+						{
+							"bg-green-600": correctLetters.indexOf(1) !== -1,
+						},
+					),
 				)}
 			>
 				{guess.length > 1 ? guess.charAt(1) : ""}
 			</div>
 			<div
-				className={clsx(
-					"h-16 w-16 border-2 text-slate-200 text-4xl font-[1000] flex justify-center items-center select-none",
-					{ "border-gray-400": guess.length > 2 },
-					{ "border-gray-600": guess.length <= 2 },
+				className={twMerge(
+					clsx(
+						"h-16 w-16 border-2 text-slate-200 text-4xl font-[1000] flex justify-center items-center select-none",
+						{
+							"border-gray-400": guess.length > 2,
+						},
+						{
+							"border-gray-600": guess.length <= 2,
+						},
+						{
+							"bg-gray-600 border-slate-950": guess.length === 5,
+						},
+						{
+							"bg-[#B59F38]": misplacedLetters.indexOf(2) !== -1,
+						},
+						{
+							"bg-green-600": correctLetters.indexOf(2) !== -1,
+						},
+					),
 				)}
 			>
 				{guess.length > 2 ? guess.charAt(2) : ""}
 			</div>
 			<div
-				className={clsx(
-					"h-16 w-16 border-2 text-slate-200 text-4xl font-[1000] flex justify-center items-center select-none",
-					{ "border-gray-400": guess.length > 3 },
-					{ "border-gray-600": guess.length <= 3 },
+				className={twMerge(
+					clsx(
+						"h-16 w-16 border-2 text-slate-200 text-4xl font-[1000] flex justify-center items-center select-none",
+						{
+							"border-gray-400": guess.length > 3,
+						},
+						{
+							"border-gray-600": guess.length <= 3,
+						},
+						{
+							"bg-gray-600 border-slate-950": guess.length === 5,
+						},
+
+						{
+							"bg-[#B59F38]": misplacedLetters.indexOf(3) !== -1,
+						},
+						{
+							"bg-green-600": correctLetters.indexOf(3) !== -1,
+						},
+					),
 				)}
 			>
 				{guess.length > 3 ? guess.charAt(3) : ""}
 			</div>
 			<div
-				className={clsx(
-					"h-16 w-16 border-2 text-slate-200 text-4xl font-[1000] flex justify-center items-center select-none",
-					{ "border-gray-400": guess.length > 4 },
-					{ "border-gray-600": guess.length <= 4 },
+				className={twMerge(
+					clsx(
+						"h-16 w-16 border-2 text-slate-200 text-4xl font-[1000] flex justify-center items-center select-none",
+						{
+							"border-gray-400": guess.length > 4,
+						},
+						{
+							"border-gray-600": guess.length <= 4,
+						},
+						{
+							"bg-gray-600 border-slate-950": guess.length === 5,
+						},
+
+						{
+							"bg-[#B59F38]": misplacedLetters.indexOf(4) !== -1,
+						},
+						{
+							"bg-green-600": correctLetters.indexOf(4) !== -1,
+						},
+					),
 				)}
 			>
 				{guess.length > 4 ? guess.charAt(4) : ""}
--- frontend/src/routes/play.tsx
+++ frontend/src/routes/play.tsx
@@ -6,106 +6,125 @@ import { GuessBar } from "@/components/GuessBar";
 import { WordleLogo } from "@/components/WordleLogo";
 
 export const Route = createFileRoute("/play")({
-    component: Play,
+	component: Play,
 });
 
 function Play() {
-    const [guesses, setGuesses] = useState<string[]>(["", "", "", "", "", ""]);
-    const [isFetching, setIsFetching] = useState(false)
-    const [correctLetters, setCorrectLetters] = useState<Array<number>[]>([
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-    ]);
-    const [misplacedLetters, setMisplacedLetters] = useState<Array<number[]>>([
-        [],
-        [],
-        [],
-        [],
-        [],
-        [],
-    ]);
+	const [isGuessCorrect, setIsGuessCorrect] = useState(false);
+	const [guesses, setGuesses] = useState<string[]>(["", "", "", "", "", ""]);
+	const [isFetching, setIsFetching] = useState(false);
+	const [correctLetters, setCorrectLetters] = useState<number[][]>([
+		[],
+		[],
+		[],
+		[],
+		[],
+		[],
+	]);
+	const [misplacedLetters, setMisplacedLetters] = useState<number[][]>([
+		[],
+		[],
+		[],
+		[],
+		[],
+		[],
+	]);
 
-    const fetchGuess = async (guess: string) => {
-        const res = await fetch(`${apiUrl}/word`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ guess }),
-        });
-        if (!res.ok) {
-            // handle errors
-        }
-        const data = await res.json();
-        console.log(data);
-        return data;
-    };
+	const fetchGuess = async (guess: string, currentIndex: number) => {
+		const res = await fetch(`${apiUrl}/word`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ guess }),
+		});
+		if (!res.ok) {
+			// handle errors (could do a toast here)
+			return;
+		}
+		const data = await res.json();
+		setCorrectLetters((prev) => [
+			...prev.slice(0, currentIndex),
+			data.correctLetters,
+			...prev.slice(currentIndex + 1, 6),
+		]);
+		setMisplacedLetters((prev) => [
+			...prev.slice(0, currentIndex),
+			data.misplacedLetters,
+			...prev.slice(currentIndex + 1, 6),
+		]);
+		if (data.correctLetters.length === 5) {
+			setIsGuessCorrect(true);
+		}
+		setIsFetching(false);
+	};
 
-    const handleKeyDown = (e: KeyboardEvent) => {
-        const currentIndex = guesses.findIndex((guess) => guess.length < 5);
-        const ch = e.key;
-        if (
-            currentIndex === -1 ||
-            !(
-                (ch.length === 1 && ch >= "a" && ch <= "z") ||
-                (ch.length === 1 && ch >= "A" && ch <= "Z") ||
-                ch === "Backspace"
-            )
-        ) {
-            return;
-        }
+	const handleKeyDown = (e: KeyboardEvent) => {
+		const currentIndex = guesses.findIndex((guess) => guess.length < 5);
+		const ch = e.key;
+		if (
+			isGuessCorrect ||
+			currentIndex === -1 ||
+			!(
+				(ch.length === 1 && ch >= "a" && ch <= "z") ||
+				(ch.length === 1 && ch >= "A" && ch <= "Z") ||
+				ch === "Backspace"
+			)
+		) {
+			return;
+		}
 
-        setGuesses((prev) => {
-            const currentGuess = prev[currentIndex];
+		setGuesses((prev) => {
+			const currentGuess = prev[currentIndex];
 
-            if (ch === "Backspace") {
-                if (currentGuess.length > 0) {
-                    const newGuesses = [
-                        ...prev.slice(0, currentIndex),
-                        currentGuess.slice(0, currentGuess.length - 1),
-                        ...prev.slice(currentIndex + 1, prev.length),
-                    ];
-                    return newGuesses;
-                }
-                return prev;
-            }
+			if (ch === "Backspace") {
+				if (currentGuess.length > 0) {
+					const newGuesses = [
+						...prev.slice(0, currentIndex),
+						currentGuess.slice(0, currentGuess.length - 1),
+						...prev.slice(currentIndex + 1, prev.length),
+					];
+					return newGuesses;
+				}
+				return prev;
+			}
 
-            const nextGuess = currentGuess.concat(ch.toUpperCase());
-            if (nextGuess.length === 5) {
-                setIsFetching(true)
-                fetchGuess(nextGuess)
-            }
-            setIsFetching(false)
-            const newGuesses = [
-                ...prev.slice(0, currentIndex),
-                nextGuess,
-                ...prev.slice(currentIndex + 1, prev.length),
-            ];
-            return newGuesses;
-        });
-    };
+			const nextGuess = currentGuess.concat(ch.toUpperCase());
+			if (nextGuess.length === 5) {
+				setIsFetching(true);
+				fetchGuess(nextGuess, currentIndex);
+			}
+			const newGuesses = [
+				...prev.slice(0, currentIndex),
+				nextGuess,
+				...prev.slice(currentIndex + 1, prev.length),
+			];
+			return newGuesses;
+		});
+	};
 
-    useEffect(() => {
-        !isFetching && window.addEventListener("keydown", handleKeyDown);
+	useEffect(() => {
+		!isFetching && window.addEventListener("keydown", handleKeyDown);
 
-        return () => {
-            window.removeEventListener("keydown", handleKeyDown);
-        };
-    }, [handleKeyDown, isFetching]);
+		return () => {
+			window.removeEventListener("keydown", handleKeyDown);
+		};
+	}, [handleKeyDown, isFetching]);
 
-    return (
-        <div className="bg-slate-950 text-slate-300 h-screen w-screen flex flex-col justify-center items-center">
-            <WordleLogo theme="dark" />
+	return (
+		<div className="bg-slate-950 text-slate-300 h-screen w-screen flex flex-col justify-center items-center">
+			<WordleLogo theme="dark" />
 
-            <div className="flex flex-col gap-2">
-                {guesses.map((guess, i) => (
-                    <GuessBar guess={guess} key={`${i}${guess}`} />
-                ))}
-            </div>
-        </div>
-    );
+			<div className="flex flex-col gap-2">
+				{guesses.map((guess, i) => (
+					<GuessBar
+						guess={guess}
+						correctLetters={correctLetters[i]}
+						misplacedLetters={misplacedLetters[i]}
+						key={`${i}${guess}`}
+					/>
+				))}
+			</div>
+		</div>
+	);
 }
