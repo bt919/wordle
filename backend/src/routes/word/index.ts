fa14762150ee4625575adf1e9fb8fe8ee9492b6b
Hisoka <gintama2009@hotmail.ca>
2025-06-01 01:26:11 +0000

    check word against a list of valid words

--- backend/src/routes/word/index.ts
+++ backend/src/routes/word/index.ts
@@ -1,61 +1,66 @@
 import type {
-	FastifyInstance,
-	FastifyPluginOptions,
-	FastifyReply,
-	FastifyRequest,
-	HookHandlerDoneFunction,
+    FastifyInstance,
+    FastifyPluginOptions,
+    FastifyReply,
+    FastifyRequest,
+    HookHandlerDoneFunction,
 } from "fastify";
 import { Type } from "@fastify/type-provider-typebox";
 
 export function wordRoutes(
-	fastify: FastifyInstance,
-	opts: FastifyPluginOptions,
-	done: HookHandlerDoneFunction,
+    fastify: FastifyInstance,
+    opts: FastifyPluginOptions,
+    done: HookHandlerDoneFunction,
 ) {
-	fastify.post(
-		"/word",
-		{
-			schema: {
-				body: Type.Object({
-					guess: Type.String({
-						minLength: 5,
-						maxLength: 5,
-					}),
-				}),
-			},
-		},
-		async (
-			request: FastifyRequest<{ Body: { guess: string } }>,
-			reply: FastifyReply,
-		) => {
-			try {
-				const { guess } = request.body;
+    fastify.post(
+        "/word",
+        {
+            schema: {
+                body: Type.Object({
+                    guess: Type.String({
+                        minLength: 5,
+                        maxLength: 5,
+                    }),
+                }),
+            },
+        },
+        async (
+            request: FastifyRequest<{ Body: { guess: string } }>,
+            reply: FastifyReply,
+        ) => {
+            try {
+                const { guess } = request.body;
 
-				const result = await fastify.query(
-					"SELECT word FROM words WHERE word_date = NOW()::date",
-				);
-				const word = result.rows[0].word;
+                const isWordValid = await fastify.query("SELECT 1 FROM allowed_words WHERE $1 ILIKE word", [guess])
+                if (isWordValid.rowCount === 0) {
+                    return reply.status(409).send({})
+                }
 
-				const correctLetters = [];
-				const misplacedLetters = [];
-				for (let i = 0; i < guess.length; i += 1) {
-					const ch = guess.charAt(i).toLowerCase();
-					if (ch === word.charAt(i)) {
-						correctLetters.push(i);
-					} else if (word.indexOf(ch) !== -1) {
-						misplacedLetters.push(i);
-					}
-				}
+                const result = await fastify.query(
+                    "SELECT word FROM words WHERE word_date = NOW()::date",
+                );
+                const word = result.rows[0].word;
 
-				return reply
-					.status(200)
-					.send({ correctLetters, misplacedLetters, guess });
-			} catch (ex) {
-				fastify.log.error(ex);
-				return reply.status(500).send({});
-			}
-		},
-	);
+                const correctLetters = [];
+                const misplacedLetters = [];
+                for (let i = 0; i < guess.length; i += 1) {
+                    const ch = guess.charAt(i).toLowerCase();
+                    if (ch === word.charAt(i)) {
+                        correctLetters.push(i);
+                    } else if (word.indexOf(ch) !== -1) {
+                        misplacedLetters.push(i);
+                    }
+                }
 
-	done();
+                return reply
+                    .status(200)
+                    .send({ correctLetters, misplacedLetters, guess });
+            } catch (ex) {
+                fastify.log.error(ex);
+                return reply.status(500).send({});
+            }
+        },
+    );
+
+    done();
 }
