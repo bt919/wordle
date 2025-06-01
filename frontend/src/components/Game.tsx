53baef618623ca38a024bcc0c5af81206e7e9d27
Hisoka <gintama2009@hotmail.ca>
2025-06-01 22:59:06 +0000

    turn off animation for users using reduced motion

--- frontend/src/components/Game.tsx
+++ frontend/src/components/Game.tsx
@@ -9,196 +9,196 @@ import { Keyboard } from "@/components/Keyboard";
 import { WordleLogo } from "@/components/WordleLogo";
 
 export function TestComponent() {
-	return (
-		<div>
-			<h1>test component</h1>
-		</div>
-	);
+    return (
+        <div>
+            <h1>test component</h1>
+        </div>
+    );
 }
 
 export function Game({ wordLength }: { wordLength?: number }) {
-	const [showErrorMessage, setShowErrorMessage] = useState(false);
-	const [isGuessesComplete, setIsGuessesComplete] = useState(
-		new Array(6).fill(false),
-	);
-	const [isGuessCorrect, setIsGuessCorrect] = useState(false);
-	const [guesses, setGuesses] = useState<string[]>(["", "", "", "", "", ""]);
-	const [isFetching, setIsFetching] = useState(false);
-	const [correctLetters, setCorrectLetters] = useState<number[][]>([
-		[],
-		[],
-		[],
-		[],
-		[],
-		[],
-	]);
-	const [misplacedLetters, setMisplacedLetters] = useState<number[][]>([
-		[],
-		[],
-		[],
-		[],
-		[],
-		[],
-	]);
+    const [showErrorMessage, setShowErrorMessage] = useState(false);
+    const [isGuessesComplete, setIsGuessesComplete] = useState(
+        new Array(6).fill(false),
+    );
+    const [isGuessCorrect, setIsGuessCorrect] = useState(false);
+    const [guesses, setGuesses] = useState<string[]>(["", "", "", "", "", ""]);
+    const [isFetching, setIsFetching] = useState(false);
+    const [correctLetters, setCorrectLetters] = useState<number[][]>([
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+    ]);
+    const [misplacedLetters, setMisplacedLetters] = useState<number[][]>([
+        [],
+        [],
+        [],
+        [],
+        [],
+        [],
+    ]);
 
-	const fetchGuess = async (guess: string, currentIndex: number) => {
-		const res = await fetch(`${apiUrl}/word`, {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({ guess }),
-		});
-		if (!res.ok) {
-			setShowErrorMessage(true);
-			setTimeout(() => {
-				setShowErrorMessage(false);
-			}, 1000);
-			// handle errors (could do a toast here)
-			return;
-		}
-		const data = await res.json();
-		setCorrectLetters((prev) => [
-			...prev.slice(0, currentIndex),
-			data.correctLetters,
-			...prev.slice(currentIndex + 1, 6),
-		]);
-		setMisplacedLetters((prev) => [
-			...prev.slice(0, currentIndex),
-			data.misplacedLetters,
-			...prev.slice(currentIndex + 1, 6),
-		]);
-		if (data.correctLetters.length === 5) {
-			setIsGuessCorrect(true);
-		}
-		setIsGuessesComplete((prev) => [
-			...prev.slice(0, currentIndex),
-			true,
-			...prev.slice(currentIndex + 1, 6),
-		]);
+    const fetchGuess = async (guess: string, currentIndex: number) => {
+        const res = await fetch(`${apiUrl}/word`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ guess }),
+        });
+        if (!res.ok) {
+            setShowErrorMessage(true);
+            setTimeout(() => {
+                setShowErrorMessage(false);
+            }, 1000);
+            // handle errors (could do a toast here)
+            return;
+        }
+        const data = await res.json();
+        setCorrectLetters((prev) => [
+            ...prev.slice(0, currentIndex),
+            data.correctLetters,
+            ...prev.slice(currentIndex + 1, 6),
+        ]);
+        setMisplacedLetters((prev) => [
+            ...prev.slice(0, currentIndex),
+            data.misplacedLetters,
+            ...prev.slice(currentIndex + 1, 6),
+        ]);
+        if (data.correctLetters.length === 5) {
+            setIsGuessCorrect(true);
+        }
+        setIsGuessesComplete((prev) => [
+            ...prev.slice(0, currentIndex),
+            true,
+            ...prev.slice(currentIndex + 1, 6),
+        ]);
 
-		setTimeout(() => {
-			setIsFetching(false);
-		}, 1500);
-		// setIsFetching(false);
-	};
+        setTimeout(() => {
+            setIsFetching(false);
+        }, 1500);
+        // setIsFetching(false);
+    };
 
-	const handleKeyDown = (e: KeyboardEvent) => {
-		if (e.repeat) {
-			return;
-		}
+    const handleKeyDown = (e: KeyboardEvent) => {
+        if (e.repeat) {
+            return;
+        }
 
-		const currentIndex = guesses.findIndex((guess) => guess.length < 5);
-		const key = e.key;
+        const currentIndex = guesses.findIndex((guess) => guess.length < 5);
+        const key = e.key;
 
-		// trigger fetch only when "Enter" is hit
-		if (isFetching) {
-			const lastIndex = currentIndex === -1 ? 5 : currentIndex - 1;
-			if (key === "Backspace") {
-				setGuesses((prev) => {
-					return [
-						...prev.slice(0, lastIndex),
-						prev[lastIndex].slice(0, 4),
-						...prev.slice(lastIndex + 1, prev.length),
-					];
-				});
-				setIsFetching(false);
-				return;
-			}
+        // trigger fetch only when "Enter" is hit
+        if (isFetching) {
+            const lastIndex = currentIndex === -1 ? 5 : currentIndex - 1;
+            if (key === "Backspace") {
+                setGuesses((prev) => {
+                    return [
+                        ...prev.slice(0, lastIndex),
+                        prev[lastIndex].slice(0, 4),
+                        ...prev.slice(lastIndex + 1, prev.length),
+                    ];
+                });
+                setIsFetching(false);
+                return;
+            }
 
-			if (key !== "Enter") {
-				return;
-			}
+            if (key !== "Enter") {
+                return;
+            }
 
-			fetchGuess(guesses[lastIndex], lastIndex);
-			return;
-		}
+            fetchGuess(guesses[lastIndex], lastIndex);
+            return;
+        }
 
-		if (
-			isGuessCorrect ||
-			currentIndex === -1 ||
-			!(
-				(key.length === 1 && key >= "a" && key <= "z") ||
-				(key.length === 1 && key >= "A" && key <= "Z") ||
-				key === "Backspace"
-			)
-		) {
-			return;
-		}
+        if (
+            isGuessCorrect ||
+            currentIndex === -1 ||
+            !(
+                (key.length === 1 && key >= "a" && key <= "z") ||
+                (key.length === 1 && key >= "A" && key <= "Z") ||
+                key === "Backspace"
+            )
+        ) {
+            return;
+        }
 
-		setGuesses((prev) => {
-			const currentGuess = prev[currentIndex];
+        setGuesses((prev) => {
+            const currentGuess = prev[currentIndex];
 
-			if (key === "Backspace") {
-				if (currentGuess.length > 0) {
-					const newGuesses = [
-						...prev.slice(0, currentIndex),
-						currentGuess.slice(0, currentGuess.length - 1),
-						...prev.slice(currentIndex + 1, prev.length),
-					];
-					return newGuesses;
-				}
-				return prev;
-			}
+            if (key === "Backspace") {
+                if (currentGuess.length > 0) {
+                    const newGuesses = [
+                        ...prev.slice(0, currentIndex),
+                        currentGuess.slice(0, currentGuess.length - 1),
+                        ...prev.slice(currentIndex + 1, prev.length),
+                    ];
+                    return newGuesses;
+                }
+                return prev;
+            }
 
-			const nextGuess = currentGuess.concat(key.toUpperCase());
-			if (nextGuess.length === 5) {
-				setIsFetching(true);
-			}
-			const newGuesses = [
-				...prev.slice(0, currentIndex),
-				nextGuess,
-				...prev.slice(currentIndex + 1, prev.length),
-			];
-			return newGuesses;
-		});
-	};
+            const nextGuess = currentGuess.concat(key.toUpperCase());
+            if (nextGuess.length === 5) {
+                setIsFetching(true);
+            }
+            const newGuesses = [
+                ...prev.slice(0, currentIndex),
+                nextGuess,
+                ...prev.slice(currentIndex + 1, prev.length),
+            ];
+            return newGuesses;
+        });
+    };
 
-	useEffect(() => {
-		console.log("useEffect fired from play.tsx");
-		window.addEventListener("keydown", handleKeyDown);
+    useEffect(() => {
+        console.log("useEffect fired from play.tsx");
+        window.addEventListener("keydown", handleKeyDown);
 
-		return () => {
-			window.removeEventListener("keydown", handleKeyDown);
-		};
-	}, [isFetching, handleKeyDown]);
+        return () => {
+            window.removeEventListener("keydown", handleKeyDown);
+        };
+    }, [isFetching, handleKeyDown]);
 
-	return (
-		<div className="min-h-screen bg-slate-950 text-slate-300 flex flex-col justify-center items-center relative">
-			<WordleLogo theme="dark" />
+    return (
+        <div className="min-h-screen bg-slate-950 text-slate-300 flex flex-col justify-center items-center relative">
+            <WordleLogo theme="dark" />
 
-			<div
-				className={twMerge(
-					clsx(
-						"absolute bg-slate-100 p-2 rounded-lg top-[100px] sm:top-[140px] invisible",
-						{
-							visible: showErrorMessage,
-						},
-					),
-				)}
-			>
-				<p className="text-slate-900 font-bold">Not in word list</p>
-			</div>
+            <div
+                className={twMerge(
+                    clsx(
+                        "absolute bg-slate-100 p-2 rounded-lg top-[100px] sm:top-[140px] invisible",
+                        {
+                            visible: showErrorMessage,
+                        },
+                    ),
+                )}
+            >
+                <p className="text-slate-900 font-bold">Not in word list</p>
+            </div>
 
-			<div className="flex flex-col gap-2">
-				{guesses.map((guess, i) => (
-					<GuessBar
-						isGuessComplete={isGuessesComplete[i]}
-						guess={guess}
-						correctLetters={correctLetters[i]}
-						misplacedLetters={misplacedLetters[i]}
-						key={`${i}${guess}`}
-						cn={showErrorMessage ? "animate-shake" : ""}
-						wordLength={5}
-					/>
-				))}
-			</div>
+            <div className="flex flex-col gap-2">
+                {guesses.map((guess, i) => (
+                    <GuessBar
+                        isGuessComplete={isGuessesComplete[i]}
+                        guess={guess}
+                        correctLetters={correctLetters[i]}
+                        misplacedLetters={misplacedLetters[i]}
+                        key={`${i}${guess}`}
+                        cn={showErrorMessage ? "motion-safe:animate-shake" : ""}
+                        wordLength={5}
+                    />
+                ))}
+            </div>
 
-			<Keyboard
-				correctLetters={correctLetters}
-				misplacedLetters={misplacedLetters}
-				guesses={guesses}
-			/>
-		</div>
-	);
+            <Keyboard
+                correctLetters={correctLetters}
+                misplacedLetters={misplacedLetters}
+                guesses={guesses}
+            />
+        </div>
+    );
 }
--- frontend/src/components/GuessBar.tsx
+++ frontend/src/components/GuessBar.tsx
@@ -35,34 +35,34 @@ export function GuessBar({
 									"border-gray-500": guess.length > index,
 								},
 								{
-									"animate-popout":
+									"motion-safe:animate-popout":
 										guess.length === index + 1 && !isGuessComplete,
 								},
 								{
 									"border-gray-600": guess.length <= index,
 								},
 								{
-									"transition delay-[400ms] animate-[xRotate_700ms_ease-in-out]":
+									"transition delay-[400ms] motion-safe:animate-[xRotate_700ms_ease-in-out]":
 										isGuessComplete && index === 0,
-									"transition delay-[700ms] animate-[xRotate_700ms_ease-in-out_400ms]":
+									"transition delay-[700ms] motion-safe:animate-[xRotate_700ms_ease-in-out_400ms]":
 										isGuessComplete && index === 1,
-									"transition delay-[1000ms] animate-[xRotate_700ms_ease-in-out_700ms]":
+									"transition delay-[1000ms] motion-safe:animate-[xRotate_700ms_ease-in-out_700ms]":
 										isGuessComplete && index === 2,
-									"transition delay-[1300ms] animate-[xRotate_700ms_ease-in-out_1000ms]":
+									"transition delay-[1300ms] motion-safe:animate-[xRotate_700ms_ease-in-out_1000ms]":
 										isGuessComplete && index === 3,
-									"transition delay-[1600ms] animate-[xRotate_700ms_ease-in-out_1300ms]":
+									"transition delay-[1600ms] motion-safe:animate-[xRotate_700ms_ease-in-out_1300ms]":
 										isGuessComplete && index === 4,
-									"transition delay-[1900ms] animate-[xRotate_700ms_ease-in-out_1600ms]":
+									"transition delay-[1900ms] motion-safe:animate-[xRotate_700ms_ease-in-out_1600ms]":
 										isGuessComplete && index === 5,
-									"transition delay-[2200ms] animate-[xRotate_700ms_ease-in-out_1900ms]":
+									"transition delay-[2200ms] motion-safe:animate-[xRotate_700ms_ease-in-out_1900ms]":
 										isGuessComplete && index === 6,
-									"transition delay-[2500ms] animate-[xRotate_700ms_ease-in-out_2200ms]":
+									"transition delay-[2500ms] motion-safe:animate-[xRotate_700ms_ease-in-out_2200ms]":
 										isGuessComplete && index === 7,
-									"transition delay-[2800ms] animate-[xRotate_700ms_ease-in-out_2500ms]":
+									"transition delay-[2800ms] motion-safe:animate-[xRotate_700ms_ease-in-out_2500ms]":
 										isGuessComplete && index === 8,
-									"transition delay-[3100ms] animate-[xRotate_700ms_ease-in-out_2800ms]":
+									"transition delay-[3100ms] motion-safe:animate-[xRotate_700ms_ease-in-out_2800ms]":
 										isGuessComplete && index === 9,
-									"transition delay-[3400ms] animate-[xRotate_700ms_ease-in-out_3100ms]":
+									"transition delay-[3400ms] motion-safe:animate-[xRotate_700ms_ease-in-out_3100ms]":
 										isGuessComplete && index === 10,
 								},
 								{
